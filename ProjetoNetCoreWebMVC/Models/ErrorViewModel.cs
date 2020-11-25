7538583f39c01b13f20364776f88cf43c4c8ff43
thiersfb <thiers.fb@gmail.com>
2020-11-24 21:32:22 -0300

    Projeto criado

---
+++ ProjetoNetCoreWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoNetCoreWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
