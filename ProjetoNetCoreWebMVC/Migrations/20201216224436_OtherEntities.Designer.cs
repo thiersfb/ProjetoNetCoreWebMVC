fe7d4f85b526262765437688d823c61b065f02b9
thiersfb <thiers.fb@gmail.com>
2020-12-16 19:47:09 -0300

    Outras entidades e segunda migração

--- ProjetoNetCoreWebMVC/Migrations/20201216224436_OtherEntities.Designer.cs
+++ ProjetoNetCoreWebMVC/Migrations/20201216224436_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoNetCoreWebMVC.Data;
 
 namespace ProjetoNetCoreWebMVC.Migrations
 {
     [DbContext(typeof(ProjetoNetCoreWebMVCContext))]
-    partial class ProjetoNetCoreWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20201216224436_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ProjetoNetCoreWebMVC/Models/Department.cs
+++ ProjetoNetCoreWebMVC/Models/Department.cs
@@ -1,6 +1,6 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 //using System.Threading.Tasks;
 
 namespace ProjetoNetCoreWebMVC.Models
@@ -9,5 +9,27 @@ namespace ProjetoNetCoreWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(SalesRecord salesRecord)
+        {
+
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
