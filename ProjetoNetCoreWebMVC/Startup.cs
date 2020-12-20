60a9a2d34c7d78ed01efeda21b16f5b1f90d378a
thiersfb <thiers.fb@gmail.com>
2020-12-20 09:07:24 -0300

    Serviço de população de dados

--- ProjetoNetCoreWebMVC/Startup.cs
+++ ProjetoNetCoreWebMVC/Startup.cs
@@ -40,14 +40,19 @@ namespace ProjetoNetCoreWebMVC
                     //options.UseSqlServer(Configuration.GetConnectionString("ProjetoNetCoreWebMVCContext")));
                     options.UseMySql(Configuration.GetConnectionString("ProjetoNetCoreWebMVCContext"), builder => //necessário instalar o provider do banco a ser utilizado
                     builder.MigrationsAssembly("ProjetoNetCoreWebMVC")));
+
+            services.AddScoped<SeedingService>();   //registra no sistema de injeção de independencia
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
