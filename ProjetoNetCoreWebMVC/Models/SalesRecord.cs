f64f5cfdf3a0237a4d61328dd05e5b5957bbab34
thiersfb <thiers.fb@gmail.com>
2020-12-29 10:10:33 -0300

    Implementando busca agrupada

--- ProjetoNetCoreWebMVC/Models/SalesRecord.cs
+++ ProjetoNetCoreWebMVC/Models/SalesRecord.cs
@@ -1,15 +1,18 @@
 ﻿using ProjetoNetCoreWebMVC.Models.Enums;
 using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ProjetoNetCoreWebMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
--- ProjetoNetCoreWebMVC/Services/SalesRecordService.cs
+++ ProjetoNetCoreWebMVC/Services/SalesRecordService.cs
@@ -38,5 +38,26 @@ namespace ProjetoNetCoreWebMVC.Services
 
         }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            //include realiza o inner join entre as tabelas
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)  //requer que o metodo seja do tipo IGrouping
+                .ToListAsync();
+        }
+
     }
 }
